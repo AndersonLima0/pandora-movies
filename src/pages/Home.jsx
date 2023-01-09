d160b2f8327f4b9ad88f07179d7e21aef00d61cd
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2023-01-09 17:24:32 -0300

    Alimetando meu stae com a API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,24 @@
+import { useState,useEffect } from "react";
+
+//importando variaveis de env
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () =>{
+    const [topMovies, setTopMovies] = useState([])
+
+    const getTopRatedMovies = async(url) =>{
+        const res = await fetch(url)
+        const data = await res.json()
+
+        console.log(data)
+    }
+
+    useEffect(() => {
+        const topRateURL = `${moviesURL}top_rated?${apiKey}`
+        getTopRatedMovies(topRateURL)
+    }, [])
+
     return(
         <div>
             Home

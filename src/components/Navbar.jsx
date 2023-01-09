5e83bb0babe30af888ec9a2d5d205bcb59794d13
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2023-01-09 18:34:32 -0300

    Estilizando a página inicial

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi'
+import './Navbar.css';
 
 const Navbar = () => {
   return (
